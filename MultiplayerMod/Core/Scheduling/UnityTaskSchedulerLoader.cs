e45186cc36943985870195b7b1a1b3f910bfbde0
Denis Pakhorukov <xpolycone@gmail.com>
2023-03-12 01:36:07 +0200

    Add task scheduler with Unity support (#37)

---
+++ MultiplayerMod/Core/Scheduling/UnityTaskSchedulerLoader.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using MultiplayerMod.Core.Dependency;
+using MultiplayerMod.Core.Loader;
+using UnityEngine;
+
+namespace MultiplayerMod.Core.Scheduling;
+
+public class UnityTaskSchedulerLoader : IModComponentLoader {
+
+    private Logging.Logger log = new(typeof(UnityTaskSchedulerLoader));
+
+    public void OnLoad(Harmony harmony) {
+        log.Debug("Creating task scheduler...");
+        Container.Register<UnityTaskScheduler>();
+        CreateTaskExecutor();
+    }
+
+    private void CreateTaskExecutor() {
+        var gameObject = new GameObject();
+        gameObject.AddComponent<UnityTaskExecutor>();
+        Object.DontDestroyOnLoad(gameObject);
+    }
+
+}
---
+++ MultiplayerMod/Core/Scheduling/UntiyTaskScheduler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiplayerMod.Core.Scheduling;
+
+public class UnityTaskScheduler : TaskScheduler {
+
+    private readonly ConcurrentQueue<Task> tasks = new();
+
+    protected override void QueueTask(Task task) {
+        tasks.Enqueue(task);
+    }
+
+    protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued) {
+        return false;
+    }
+
+    protected override IEnumerable<Task> GetScheduledTasks() {
+        return tasks;
+    }
+
+    public void Tick() {
+        foreach (var task in tasks) {
+            TryExecuteTask(task);
+        }
+    }
+
+    public void Run(System.Action action) => tasks.Enqueue(
+        Task.Factory.StartNew(action, CancellationToken.None, TaskCreationOptions.None, this)
+    );
+
+}
