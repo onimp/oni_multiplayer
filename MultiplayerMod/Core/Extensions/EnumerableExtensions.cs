7dca09635ef3aaa69c722eaab20e3da8ef861eb3
Denis Pakhorukov <xpolycone@gmail.com>
2023-03-12 01:34:50 +0200

    Disable performance analysis for ForEach extension (#37)

--- MultiplayerMod/Core/Extensions/EnumerableExtensions.cs
+++ MultiplayerMod/Core/Extensions/EnumerableExtensions.cs
@@ -5,6 +5,7 @@ namespace MultiplayerMod.Core.Extensions;
 
 public static class EnumerableExtensions {
 
+    // ReSharper disable Unity.PerformanceAnalysis
     public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action) {
         foreach (var item in enumerable) {
             action(item);
