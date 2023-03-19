ef46ffe20314fa179254b01ba13c97cb5ec994e0
Denis Pakhorukov <xpolycone@gmail.com>
2023-03-19 12:28:46 +0200

    Add Unity object tools (#37)

---
+++ MultiplayerMod/Core/Unity/UnityObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace MultiplayerMod.Core.Unity;
+
+public abstract class UnityObject {
+
+    public static GameObject CreateStaticWithComponent<T>() where T : Component {
+        var gameObject = new GameObject();
+        gameObject.AddComponent<T>();
+        Object.DontDestroyOnLoad(gameObject);
+        return gameObject;
+    }
+
+    public static void Destroy(GameObject gameObject) {
+        Object.Destroy(gameObject);
+    }
+
+}
