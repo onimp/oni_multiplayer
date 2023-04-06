a6cea9d1b946007a84c20feaa1586e02c4d3d4f4
Denis Pakhorukov <xpolycone@gmail.com>
2023-04-07 01:50:24 +0300

    Add game join action to platform dependent operations (#37)

--- MultiplayerMod/Platform/Steam/SteamPlatformLoader.cs
+++ MultiplayerMod/Platform/Steam/SteamPlatformLoader.cs
@@ -2,6 +2,7 @@
 using MultiplayerMod.Core.Dependency;
 using MultiplayerMod.Core.Loader;
 using MultiplayerMod.Core.Unity;
+using MultiplayerMod.Multiplayer;
 using MultiplayerMod.Network;
 using MultiplayerMod.Platform.Steam.Network;
 using MultiplayerMod.Platform.Steam.Network.Components;
@@ -18,6 +19,7 @@ public class SteamPlatformLoader : IModComponentLoader {
             return;
 
         log.Info("Steam platform detected");
+        Container.Register<IMultiplayerOperations, SteamMultiplayerOperations>();
         Container.Register<IMultiplayerServer, SteamServer>();
         Container.Register<IMultiplayerClient, SteamClient>();
         Container.Register<SteamLobby>();
