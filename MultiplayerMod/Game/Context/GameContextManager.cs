6a07cbeca47456af60cfe59d98ce7bb011d05859
Denis Pakhorukov <xpolycone@gmail.com>
2023-04-15 01:58:15 +0300

    Add game context switching support
    Required to set some values, like priority, stored in global instances.

---
+++ MultiplayerMod/Game/Context/GameContextManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MultiplayerMod.Game.Context;
+
+public abstract class GameContextManager {
+
+    private static readonly GameContextOverride contextOverride = new();
+
+    public static void Override(OverrideContext context, System.Action action) {
+        Override<object>(
+            context,
+            () => {
+                action();
+                return null;
+            }
+        );
+    }
+
+    public static T Override<T>(OverrideContext context, Func<T> action) {
+        var originalContext = GameContext.GetCurrent();
+        try {
+            contextOverride.Restore();
+            if (context.Priority != null)
+                contextOverride.PriorityScreen.lastSelectedPriority = context.Priority.Value;
+            return action();
+        } finally {
+            originalContext.Restore();
+        }
+    }
+
+}
---
+++ MultiplayerMod/Game/Context/GameContextOverride.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiplayerMod.Game.Context;
+
+// ReSharper disable Unity.IncorrectMonoBehaviourInstantiation
+public class GameContextOverride : GameContext {
+
+    public PriorityScreen PriorityScreen { get; set; }
+
+    public GameContextOverride() {
+        ToolMenu = new ToolMenu();
+        PriorityScreen = new PriorityScreen();
+        ToolMenu.priorityScreen = PriorityScreen;
+    }
+
+}
