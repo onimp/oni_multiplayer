5283efc4c502cde11706be891168aaeea149f4b1
Denis Pakhorukov <xpolycone@gmail.com>
2023-04-17 22:48:12 +0300

    Add copy settings tool sync
    Despite of copy settings tool does work itself it requires building parameters sync to work as expected.

---
+++ MultiplayerMod/Multiplayer/Commands/Tools/CopySettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MultiplayerMod.Core.Logging;
+using MultiplayerMod.Game.Effects;
+using MultiplayerMod.Game.Events.Tools;
+
+namespace MultiplayerMod.Multiplayer.Commands.Tools;
+
+[Serializable]
+public class CopySettings : IMultiplayerCommand {
+
+    private static readonly Core.Logging.Logger log = LoggerFactory.GetLogger<CopySettings>();
+
+    private CopySettingsEventArgs @event;
+
+    public CopySettings(CopySettingsEventArgs @event) {
+        this.@event = @event;
+    }
+
+    public void Execute() {
+        try {
+            PopUpEffects.Enabled = false;
+            DoExecute();
+        } finally {
+            PopUpEffects.Enabled = true;
+        }
+    }
+
+    // ReSharper disable once Unity.IncorrectMonoBehaviourInstantiation
+    private void DoExecute() {
+        var tool = new CopySettingsTool();
+        var source = Grid.Objects[@event.SourceCell, (int) @event.SourceLayer];
+        if (source == null) {
+            log.Warning($"Unable to locate source at cell #{@event.SourceCell} on {@event.SourceLayer} layer");
+            return;
+        }
+        tool.SetSourceObject(source);
+        @event.DragEvent.Cells.ForEach(it => tool.OnDragTool(it, 0));
+    }
+
+}
--- MultiplayerMod/Multiplayer/Patches/DragToolEffect.cs
+++ MultiplayerMod/Multiplayer/Patches/DragToolEffect.cs
@@ -20,60 +20,6 @@ namespace MultiplayerMod.Multiplayer.Patches
             var currentPriority = ToolMenu.Instance.PriorityScreen.GetLastSelectedPriority();
             var newPriority = new PrioritySetting(PriorityScreen.PriorityClass.basic, priority);
 
-            switch (dragTool)
-            {
-                case BuildTool buildTool:
-                {
-                    PlanScreen.Instance.ProductInfoScreen.Awake();
-                    PlanScreen.Instance.ProductInfoScreen.materialSelectionPanel.OnPrefabInit();
-                    var additionalObject = (object[])array[3];
-
-                    var def = Assets.GetBuildingDef((string)additionalObject[0]);
-                    var facadeId = (string)additionalObject[1];
-                    var selectedElements = (IList<Tag>)additionalObject[2];
-                    var orientation = (Orientation)additionalObject[3];
-                    if (def.isKAnimTile && def.isUtility)
-                    {
-                        (def.BuildingComplete.GetComponent<Wire>() != null
-                            ? WireBuildTool.Instance
-                            : (BaseUtilityBuildTool)UtilityBuildTool.Instance).Activate(def, selectedElements);
-                    }
-                    else
-                    {
-                        buildTool.Activate(def, selectedElements, facadeId);
-                        buildTool.SetToolOrientation(orientation);
-                        buildTool.UpdateVis(Grid.CellToPosCCC(cell, Grid.SceneLayer.Building));
-                    }
-
-                    PlanScreen.Instance.ProductInfoScreen.materialSelectionPanel.PriorityScreen.SetScreenPriority(
-                        newPriority);
-                    break;
-                }
-                case BaseUtilityBuildTool baseUtilityBuildTool:
-                {
-                    var additionalObject = (object[])array[3];
-                    var def = Assets.GetBuildingDef((string)additionalObject[0]);
-                    var selectedElements = (IList<Tag>)additionalObject[1];
-                    var pathCells = (List<int>)additionalObject[2];
-
-                    baseUtilityBuildTool.Activate(def, selectedElements);
-
-                    var path = baseUtilityBuildTool.path;
-                    path.Clear();
-                    pathCells.ForEach(pathCell =>
-                    {
-                        var pathNode = new BaseUtilityBuildTool.PathNode { cell = pathCell };
-                        path.Add(pathNode);
-                    });
-
-                    PlanScreen.Instance.ProductInfoScreen.Awake();
-                    PlanScreen.Instance.ProductInfoScreen.materialSelectionPanel.OnPrefabInit();
-                    PlanScreen.Instance.ProductInfoScreen.materialSelectionPanel.PriorityScreen.SetScreenPriority(
-                        newPriority);
-                    break;
-                }
-            }
-
             ToolMenu.Instance.PriorityScreen.SetScreenPriority(newPriority);
 
             if (dragTool is BaseUtilityBuildTool tool)
