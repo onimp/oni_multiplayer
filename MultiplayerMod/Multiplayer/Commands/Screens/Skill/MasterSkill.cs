dbd86a6ff19eb06144c97fe5fb1da81adf92ec39
Zuev Vladimir <zuev93@mail.ru>
2023-06-03 10:37:10 +0200

    #81 Sync skills and hats.

---
+++ MultiplayerMod/Multiplayer/Commands/Screens/Skill/MasterSkill.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MultiplayerMod.Multiplayer.Extensions;
+
+namespace MultiplayerMod.Multiplayer.Commands.Screens.Skill;
+
+[Serializable]
+public class MasterSkill : IMultiplayerCommand {
+
+    private readonly string properName;
+    private readonly string skillId;
+
+    public MasterSkill(string properName, string skillId) {
+        this.properName = properName;
+        this.skillId = skillId;
+    }
+
+    public void Execute() {
+        var minionIdentity = MinionIdentityUtils.GetLiveMinion(properName);
+
+        var component = minionIdentity.GetComponent<MinionResume>();
+        if (DebugHandler.InstantBuildMode && component.AvailableSkillpoints < 1)
+            component.ForceAddSkillPoint();
+        var masteryConditions = component.GetSkillMasteryConditions(skillId);
+        if (!component.CanMasterSkill(masteryConditions)) return;
+        if (component.HasMasteredSkill(skillId)) return;
+
+        component.MasterSkill(skillId);
+
+        ManagementMenu.Instance.skillsScreen.RefreshAll();
+    }
+}
