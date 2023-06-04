aafb8c3b89e17a9c14c3fe667362b5469965d9ef
Zuev Vladimir <zuev93@mail.ru>
2023-06-04 12:03:28 +0200

    #21 Sync user menu side buttons.
    All buttons should be synced except: CopySettings, MoveToLocation, Show navigation, Follow cam

---
+++ MultiplayerMod/Multiplayer/Commands/Screens/UserMenu/ClickUserMenuButton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using MultiplayerMod.Core.Logging;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace MultiplayerMod.Multiplayer.Commands.Screens.UserMenu;
+
+[Serializable]
+public class ClickUserMenuButton : IMultiplayerCommand {
+
+    private static readonly Core.Logging.Logger log = LoggerFactory.GetLogger(typeof(ClickUserMenuButton));
+
+    private int instanceId;
+    private Type actionDeclaringType;
+    private string actionName;
+
+    public ClickUserMenuButton(GameObject gameObject, System.Action action) {
+        instanceId = gameObject.GetComponent<KPrefabID>().InstanceID;
+        actionDeclaringType = action.Method.DeclaringType;
+        actionName = action.Method.Name;
+    }
+
+    public void Execute() {
+        var kPrefabID = Object.FindObjectsOfType<KPrefabID>().FirstOrDefault(a => a.InstanceID == instanceId);
+        if (kPrefabID == null) return;
+
+        var actionObject = kPrefabID.GetComponent(actionDeclaringType);
+        if (actionObject == null) return;
+
+        try {
+            var methodInfo = actionDeclaringType.GetMethod(
+                actionName,
+                BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly,
+                null,
+                new Type[] { },
+                new ParameterModifier[] { }
+            );
+            methodInfo?.Invoke(actionObject, new object[] { });
+        } catch (Exception e) {
+            log.Error(e.ToString);
+        }
+    }
+}
