7b7583258833876a94397c4f29cedcf469f02641
Zuev Vladimir <zuev93@mail.ru>
2023-06-19 23:44:28 +0200

    #30 Adjust event naming as well.

--- MultiplayerMod/Game/UI/Screens/Events/ResearchScreenEvents.cs
+++ MultiplayerMod/Game/UI/Screens/Events/ResearchScreenEvents.cs
@@ -7,18 +7,18 @@ namespace MultiplayerMod.Game.UI.Screens.Events;
 [HarmonyPatch(typeof(ResearchEntry))]
 public static class ResearchScreenEvents {
 
-    public static event Action<string> Canceled;
-    public static event Action<string> Selected;
+    public static event Action<string> Cancel;
+    public static event Action<string> Select;
 
     [HarmonyPostfix]
     [HarmonyPatch(nameof(ResearchEntry.OnResearchCanceled))]
     // ReSharper disable once UnusedMember.Local
     private static void OnResearchCanceledPatch(ResearchEntry __instance) =>
-        PatchControl.RunIfEnabled(() => Canceled?.Invoke(__instance.targetTech.Id));
+        PatchControl.RunIfEnabled(() => Cancel?.Invoke(__instance.targetTech.Id));
 
     [HarmonyPostfix]
     [HarmonyPatch(nameof(ResearchEntry.OnResearchClicked))]
     // ReSharper disable once UnusedMember.Local
     private static void OnResearchClickedPatch(ResearchEntry __instance) =>
-        PatchControl.RunIfEnabled(() => Selected?.Invoke(__instance.targetTech.Id));
+        PatchControl.RunIfEnabled(() => Select?.Invoke(__instance.targetTech.Id));
 }
