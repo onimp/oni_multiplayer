4e5e7472b62aebadbf28bafba19c6ed46308d31c
Zuev Vladimir <zuev93@mail.ru>
2023-06-22 19:53:26 +0200

    #111 Sync disinfect overlay (germ level + enable autodisinfect) (#115)
    * #111 Sync disinfect overlay (germ level + enable autodisinfect)
    
    * #Add event for input field as well.

---
+++ MultiplayerMod/Game/UI/DisinfectOverlayEvents.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using HarmonyLib;
+using MultiplayerMod.Core.Patch;
+
+namespace MultiplayerMod.Game.UI;
+
+[HarmonyPatch(typeof(DisinfectThresholdDiagram))]
+public static class DiseaseOverlayEvents {
+
+    public static event Action<int, bool> DiseaseSettingsChanged;
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(DisinfectThresholdDiagram.OnReleaseHandle))]
+    // ReSharper disable once UnusedMember.Local
+    private static void OnReleaseHandle() => PatchControl.RunIfEnabled(
+        () => {
+            DiseaseSettingsChanged?.Invoke(
+                SaveGame.Instance.minGermCountForDisinfect,
+                SaveGame.Instance.enableAutoDisinfect
+            );
+        }
+    );
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(DisinfectThresholdDiagram.ReceiveValueFromSlider))]
+    // ReSharper disable once UnusedMember.Local
+    private static void ReceiveValueFromSlider() => PatchControl.RunIfEnabled(
+        () => {
+            DiseaseSettingsChanged?.Invoke(
+                SaveGame.Instance.minGermCountForDisinfect,
+                SaveGame.Instance.enableAutoDisinfect
+            );
+        }
+    );
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(DisinfectThresholdDiagram.ReceiveValueFromInput))]
+    // ReSharper disable once UnusedMember.Local
+    private static void ReceiveValueFromInput() => PatchControl.RunIfEnabled(
+        () => {
+            DiseaseSettingsChanged?.Invoke(
+                SaveGame.Instance.minGermCountForDisinfect,
+                SaveGame.Instance.enableAutoDisinfect
+            );
+        }
+    );
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(DisinfectThresholdDiagram.OnClickToggle))]
+    // ReSharper disable once InconsistentNaming
+    // ReSharper disable once UnusedMember.Local
+    private static void OnClickToggle() => PatchControl.RunIfEnabled(
+        () => {
+            DiseaseSettingsChanged?.Invoke(
+                SaveGame.Instance.minGermCountForDisinfect,
+                SaveGame.Instance.enableAutoDisinfect
+            );
+        }
+    );
+}
