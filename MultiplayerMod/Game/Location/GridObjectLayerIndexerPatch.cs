92545fa45eb74a326e23623f620fe204e4faca50
Denis Pakhorukov <xpolycone@gmail.com>
2023-07-02 02:21:40 +0300

    #126 Add object layer extension update

---
+++ MultiplayerMod/Game/Location/GridObjectLayerIndexerPatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using MultiplayerMod.Game.Extension;
+using UnityEngine;
+
+namespace MultiplayerMod.Game.Location;
+
+// ReSharper disable once UnusedType.Global
+[HarmonyPatch(typeof(Grid.ObjectLayerIndexer))]
+public static class GridObjectLayerIndexerPatch {
+
+    // ReSharper disable once UnusedMember.Local
+    [HarmonyPostfix]
+    [HarmonyPatch("set_Item")]
+    private static void SetItemPostfix(int cell, int layer, GameObject value) {
+        if (value == null)
+            return;
+
+        var extension = value.GetComponent<GameObjectExtension>();
+        if (extension == null)
+            return;
+
+        extension.GridLayer = layer;
+    }
+
+}
