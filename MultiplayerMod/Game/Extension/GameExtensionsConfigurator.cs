40eb0a03de161289b85953acc6396d471aae655c
Denis Pakhorukov <xpolycone@gmail.com>
2023-07-02 02:20:23 +0300

    #126 Add game objects extension

---
+++ MultiplayerMod/Game/Extension/GameExtensionsConfigurator.cs
@@ -0,0 +1,12 @@
+﻿using MultiplayerMod.Game.Objects;
+
+namespace MultiplayerMod.Game.Extension;
+
+public class GameExtensionsConfigurator {
+
+    public GameExtensionsConfigurator() {
+        KInstantiateEvents.Create += gameObject => gameObject.AddComponent<GameObjectExtension>();
+        KPrefabIdEvents.Deserialize += kPrefabId => kPrefabId.gameObject.AddComponent<GameObjectExtension>();
+    }
+
+}
