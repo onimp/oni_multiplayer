e9613cb790280aa31a9081d0b2ad92a7b7c2aa2f
Denis Pakhorukov <xpolycone@gmail.com>
2023-07-02 02:23:34 +0300

    #126 Add nullable long support to dev tool entity inspector

---
+++ MultiplayerMod/Core/Unity/Dev/DevToolSceneInspectorPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using ImGuiNET;
+
+namespace MultiplayerMod.Core.Unity.Dev;
+
+[HarmonyPatch(typeof(DevToolSceneInspector))]
+public class DevToolSceneInspectorPatch {
+
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(DevToolSceneInspector.DisplayField))]
+    private static bool DisplayFieldPrefix(ref bool __result, string name, System.Type ft, ref object obj) {
+        if (ft != typeof(long?))
+            return true;
+
+        var v = (long?) obj;
+        ImGui.LabelText(name, v.Value.ToString());
+        __result = true;
+        return false;
+    }
+
+}
