bb4ed21dd82e7c1f33dddff7a38aef7b76a05104
Denis Pakhorukov <xpolycone@gmail.com>
2023-07-03 01:48:54 +0300

    #126 Add long type support to dev tool entity inspector

--- MultiplayerMod/Game/Dev/DevToolSceneInspectorPatch.cs
+++ MultiplayerMod/Game/Dev/DevToolSceneInspectorPatch.cs
@@ -1,19 +1,21 @@
 ﻿using HarmonyLib;
 using ImGuiNET;
 
-namespace MultiplayerMod.Core.Unity.Dev;
+namespace MultiplayerMod.Game.Dev;
 
+// ReSharper disable once UnusedType.Global
 [HarmonyPatch(typeof(DevToolSceneInspector))]
 public class DevToolSceneInspectorPatch {
 
+    // ReSharper disable once UnusedMember.Local
     [HarmonyPrefix]
     [HarmonyPatch(nameof(DevToolSceneInspector.DisplayField))]
     private static bool DisplayFieldPrefix(ref bool __result, string name, System.Type ft, ref object obj) {
-        if (ft != typeof(long?))
+        var longType = ft == typeof(long) || ft == typeof(ulong);
+        if (!longType)
             return true;
 
-        var v = (long?) obj;
-        ImGui.LabelText(name, v.Value.ToString());
+        ImGui.LabelText(name, obj.ToString());
         __result = true;
         return false;
     }
