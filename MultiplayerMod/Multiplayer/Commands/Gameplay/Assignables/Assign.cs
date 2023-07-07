e5c3a0e34cc6b7eb178690c1c3793fd4e7090514
Denis Pakhorukov <xpolycone@gmail.com>
2023-07-08 01:33:26 +0300

    #140 Add assignables sync

---
+++ MultiplayerMod/Multiplayer/Commands/Gameplay/Assignables/Assign.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MultiplayerMod.Multiplayer.Objects;
+
+namespace MultiplayerMod.Multiplayer.Commands.Gameplay.Assignables;
+
+[Serializable]
+public class Assign : IMultiplayerCommand {
+
+    private MultiplayerReference Target { get; }
+    private MultiplayerReference Identity { get; }
+
+    public Assign(MultiplayerReference target, MultiplayerReference identity) {
+        Target = target;
+        Identity = identity;
+    }
+
+    public void Execute() {
+        var assignable = Target.GetComponent<Assignable>();
+        var identity = Identity?.GetComponent<IAssignableIdentity>();
+        if (identity != null)
+            assignable.Assign(identity);
+        else
+            assignable.Unassign();
+    }
+
+}
