180287dd436695afffc4679ce8b1ad85ee1746be
Zuev Vladimir <zuev93@mail.ru>
2023-07-13 17:20:21 +0200

    #92 Sync filterable side screens. (#150)
    * #92 Sync filterable side screens.
    Examples:
    FoodBox - TreeFilterableSideScreen
    LiquidFilter - FilterSideScreen
    
    * #92 Address comments from the PR.

---
+++ MultiplayerMod/Multiplayer/Commands/Screens/Filters/SetFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MultiplayerMod.Multiplayer.Objects;
+
+namespace MultiplayerMod.Multiplayer.Commands.Screens.Filters;
+
+[Serializable]
+public class SetFilter : IMultiplayerCommand {
+    private readonly MultiplayerReference reference;
+    private readonly Tag tag;
+
+    public SetFilter(MultiplayerReference reference, Tag tag) {
+        this.reference = reference;
+        this.tag = tag;
+    }
+
+    public void Execute() {
+        var filterable = reference.GetComponent<Filterable>();
+        if (filterable != null) {
+            filterable.SelectedTag = tag;
+        }
+    }
+}
