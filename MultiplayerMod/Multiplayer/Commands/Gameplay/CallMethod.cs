cb080003c5aa865c42b8205064abb56aabdfb257
Zuev Vladimir <zuev93@mail.ru>
2023-07-14 16:41:09 +0200

    #92 Generalize events and commands for sidescreens.
    All those events are triggered upon a game object method call and just passing all arguments to the client where the same method being called.
    This being replaced by a single event class and a single command instead.
    
    P.S. Few events have not been replaced since they requires additional surrogates to serialize their arguments data.

---
+++ MultiplayerMod/Multiplayer/Commands/Gameplay/CallMethod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using MultiplayerMod.Game.Mechanics;
+
+namespace MultiplayerMod.Multiplayer.Commands.Gameplay;
+
+[Serializable]
+public class CallMethod : IMultiplayerCommand {
+    private readonly ObjectEvents.ObjectEventsArgs args;
+
+    public CallMethod(ObjectEvents.ObjectEventsArgs args) {
+        this.args = args;
+    }
+
+    public void Execute() {
+        var method = args.MethodType
+            .GetMethod(
+                args.MethodName,
+                BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance |
+                BindingFlags.DeclaredOnly
+            );
+        var component = args.Target.GetComponent(args.MethodType);
+        if (component != null) {
+            method?.Invoke(component, args.Args);
+        }
+    }
+}
