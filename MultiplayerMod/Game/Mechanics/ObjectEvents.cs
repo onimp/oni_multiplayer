cee269868784ae4dc593cce28c563d6e8b0d1f5a
Zuev Vladimir <zuev93@mail.ru>
2023-07-15 21:20:05 +0200

    #92 Sync more side screen objects.
    PassengerRocketModule, RocketControlStation, ICheckboxControl, SuitLocker, IThresholdSwitch,
    ISliderControl, Valve.
    
    Added support for interface as patch target type.

--- MultiplayerMod/Game/Mechanics/ObjectEvents.cs
+++ MultiplayerMod/Game/Mechanics/ObjectEvents.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using HarmonyLib;
+using MultiplayerMod.Core.Logging;
 using MultiplayerMod.Core.Patch;
 using MultiplayerMod.Multiplayer.Objects;
 using MultiplayerMod.Multiplayer.Objects.Reference;
@@ -11,31 +12,70 @@ using MultiplayerMod.Multiplayer.Objects.Reference;
 namespace MultiplayerMod.Game.Mechanics;
 
 public static class ObjectEvents {
-
     public static event Action<ObjectEventsArgs> MethodCalled;
 
-    private static readonly Dictionary<Type, string[]> MethodsForPatch = new() {
-        { typeof(Filterable), new[] { nameof(Filterable.SelectedTag) } },
-        { typeof(TreeFilterable),
+    private static readonly Core.Logging.Logger log = LoggerFactory.GetLogger(typeof(ObjectEvents));
+
+    private static readonly Dictionary<Type, string[]> methodsForPatch = new() {
+        { typeof(Filterable), new[] { nameof(Filterable.SelectedTag) } }, {
+            typeof(TreeFilterable),
             new[] { nameof(TreeFilterable.AddTagToFilter), nameof(TreeFilterable.RemoveTagFromFilter), }
         },
-        { typeof(Clinic), new[] { "ISliderControl.SetSliderValue" } },
-        { typeof(DevRadiationEmitter), new[] { nameof(ISingleSliderControl.SetSliderValue) } },
-        { typeof(EnergyGenerator), new[] { nameof(ISingleSliderControl.SetSliderValue) } },
-        { typeof(HEPBattery.Instance), new[] { nameof(ISingleSliderControl.SetSliderValue) } },
-        { typeof(HighEnergyParticleSpawner), new[] { nameof(ISingleSliderControl.SetSliderValue) } },
-        { typeof(LogicGateBuffer), new[] { nameof(ISingleSliderControl.SetSliderValue) } },
-        { typeof(LogicGateFilter), new[] { nameof(ISingleSliderControl.SetSliderValue) } },
-        { typeof(ManualGenerator), new[] { nameof(ISingleSliderControl.SetSliderValue) } },
-        { typeof(OilWellCap), new[] { nameof(ISingleSliderControl.SetSliderValue) } },
-        { typeof(Door), new[] { nameof(Door.QueueStateChange), nameof(Door.OrderUnseal) } },
-        { typeof(ComplexFabricator),
+        { typeof(Door), new[] { nameof(Door.QueueStateChange), nameof(Door.OrderUnseal) } }, {
+            typeof(ComplexFabricator),
             new[] {
                 nameof(ComplexFabricator.IncrementRecipeQueueCount),
                 nameof(ComplexFabricator.DecrementRecipeQueueCount),
                 nameof(ComplexFabricator.SetRecipeQueueCount)
             }
         },
+        { typeof(PassengerRocketModule), new[] { nameof(PassengerRocketModule.RequestCrewBoard) } },
+        { typeof(RocketControlStation), new[] { nameof(RocketControlStation.RestrictWhenGrounded) } },
+        { typeof(ICheckboxControl), new[] { nameof(ICheckboxControl.SetCheckboxValue) } },
+        { typeof(SuitLocker), new[] { nameof(SuitLocker.ConfigNoSuit), nameof(SuitLocker.ConfigRequestSuit) } }, {
+            typeof(IThresholdSwitch),
+            new[] { nameof(IThresholdSwitch.Threshold), nameof(IThresholdSwitch.ActivateAboveThreshold) }
+        },
+        { typeof(ISliderControl), new[] { nameof(ISingleSliderControl.SetSliderValue) } },
+        { typeof(Valve), new[] { nameof(Valve.ChangeFlow) } }, {
+            typeof(ReorderableBuilding),
+            new[] {
+                nameof(ReorderableBuilding.SwapWithAbove),
+                nameof(ReorderableBuilding.SwapWithBelow),
+                nameof(ReorderableBuilding.Trigger)
+            }
+        }, {
+            typeof(SingleEntityReceptacle),
+            new[] {
+                nameof(SingleEntityReceptacle.OrderRemoveOccupant),
+                nameof(SingleEntityReceptacle.CancelActiveRequest),
+                nameof(SingleEntityReceptacle.CreateOrder),
+                nameof(SingleEntityReceptacle.SetPreview)
+            }
+        },
+        // TODO uncomment lines below after deciding on field patches.
+        //  { typeof(RailGun), new[] { nameof(RailGun.launchMass) } },
+        // {
+        //     typeof(TemperatureControlledSwitch),
+        //     new[] {
+        //         nameof(TemperatureControlledSwitch.thresholdTemperature),
+        //         nameof(TemperatureControlledSwitch.activateOnWarmerThan)
+        //     }
+        // },
+        // {
+        //     typeof(LogicTimeOfDaySensor),
+        //     new[] { nameof(LogicTimeOfDaySensor.startTime), nameof(LogicTimeOfDaySensor.duration) }
+        // },
+        // {
+        //     typeof(LogicTimerSensor),
+        //     new[] {
+        //         nameof(LogicTimerSensor.displayCyclesMode),
+        //         nameof(LogicTimerSensor.onDuration),
+        //         nameof(LogicTimerSensor.offDuration),
+        //         nameof(LogicTimerSensor.timeElapsedInCurrentState),
+        //         nameof(LogicTimerSensor.ResetTimer)
+        //     }
+        // },
         // TODO uncomment lines below after creating required surrogates.
         // { typeof(Assignable), new[] { nameof(Assignable.Assign), nameof(Assignable.Unassign) } },
         // { typeof(AccessControl),
@@ -47,28 +87,54 @@ public static class ObjectEvents {
     };
 
     private static IEnumerable<MethodBase> GetTargetMethods(int argsCount) {
+        var classTypes = methodsForPatch.Keys.Where(type => type.IsClass).ToList();
+        var interfaceTypes = methodsForPatch.Keys.Where(type => type.IsInterface).ToList();
         var targetMethods = Assembly.GetAssembly(typeof(ISliderControl))
             .GetTypes()
-            .Where(type => MethodsForPatch.Keys.Contains(type))
+            .Where(
+                type => type.IsClass && (classTypes.Contains(type)
+                                         || interfaceTypes.Any(interfaceType => interfaceType.IsAssignableFrom(type)))
+            )
             .SelectMany(
-                type =>
-                    MethodsForPatch[type].Select(
+                type => {
+                    string[] methodNames;
+                    Type interfaceType = null;
+                    if (classTypes.Contains(type)) {
+                        methodNames = methodsForPatch[type];
+                    } else {
+                        interfaceType =
+                            interfaceTypes.Single(interfaceType => interfaceType.IsAssignableFrom(type));
+                        methodNames = methodsForPatch[interfaceType];
+                    }
+                    return methodNames.Select(
                         methodName => {
                             var methodInfo = type.GetMethod(
                                 methodName,
-                                BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance |
-                                BindingFlags.DeclaredOnly
+                                BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance
                             );
-                            if (methodInfo != null) return methodInfo;
+                            if (methodInfo != null) {
+                                return methodInfo;
+                            }
+                            if (interfaceType != null) {
+                                // Some overrides names prefixed by interface e.g. Clinic#ISliderControl.SetSliderValue
+                                methodInfo = type.GetMethod(
+                                    interfaceType.Name + "." + methodName,
+                                    BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance
+                                );
+                                if (methodInfo != null) return methodInfo;
+                            }
 
                             var property = type.GetProperty(
                                 methodName,
-                                BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance |
-                                BindingFlags.DeclaredOnly
+                                BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance
                             );
+                            if (property == null) {
+                                log.Error($"Method {type}.{methodName} not found");
+                            }
                             return property?.GetSetMethod();
                         }
-                    )
+                    );
+                }
             )
             .Where(method => method.GetParameters().Length == argsCount)
             .ToList();
