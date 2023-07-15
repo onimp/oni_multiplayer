57cee5b869e5a55346038ad228b4391b3ed1e957
Zuev Vladimir <zuev93@mail.ru>
2023-07-15 20:23:41 +0200

    #87 Applied and tested suggested install script. (#157)

--- docs/scripts/install.bat
+++ docs/scripts/install.bat
@@ -1,1 +1,7 @@
-xcopy /s .\MultiplayerMod %USERPROFILE%\Documents\Klei\OxygenNotIncluded\mods\Local\MultiplayerMod\
+@echo off
+
+FOR /F "tokens=2* skip=2" %%a in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "personal"') do set DocumentsFolder=%%b
+
+echo "Installing multiplayer mod in folder: %DocumentsFolder%\Klei\OxygenNotIncluded\mods\Local\MultiplayerMod\"
+xcopy /s .\MultiplayerMod %DocumentsFolder%\Klei\OxygenNotIncluded\mods\Local\MultiplayerMod\
+pause
