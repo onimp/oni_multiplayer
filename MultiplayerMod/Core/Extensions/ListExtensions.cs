2c24cd351381f23ec9eeaa7527b2bb84df3e8c35
Denis Pakhorukov <xpolycone@gmail.com>
2023-07-17 23:43:00 +0300

    Add list destructuring extensions

---
+++ MultiplayerMod/Core/Extensions/ListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace MultiplayerMod.Core.Extensions;
+
+public static class ListExtensions {
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    public static void Deconstruct<T>(this IList<T> list, out T first) {
+        first = list[0];
+    }
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    public static void Deconstruct<T>(this IList<T> list, out T first, out T second) {
+        first = list[0];
+        second = list[1];
+    }
+
+}
