316a1481df5bc625a696af2edc3253c23e37f996
Denis Pakhorukov <xpolycone@gmail.com>
2023-07-29 10:12:23 +0300

    #183 Add delayed mod loader (#184)

---
+++ MultiplayerMod/Core/Loader/LaunchInitializerPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace MultiplayerMod.Core.Loader;
+
+[HarmonyPatch(typeof(LaunchInitializer))]
+public static class LaunchInitializerPatch {
+
+    public static DelayedModLoader Loader { get; set; } = null!;
+
+    // ReSharper disable once UnusedMember.Local
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(LaunchInitializer.DeleteLingeringFiles))]
+    private static void LaunchInitializerBeforeDeleteLingeringFiles() {
+        Loader.OnLoad();
+    }
+
+}
