4f8f86d048f9248ebcc707451f13f3523554bc2d
Denis Pakhorukov <xpolycone@gmail.com>
2023-08-02 23:21:45 +0300

    #196 Fix schedule update trigger on block group change

---
+++ MultiplayerMod/Game/Mechanics/Schedules/ScheduleConditionalUpdatePatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace MultiplayerMod.Game.Mechanics.Schedules;
+
+[HarmonyPatch(typeof(Schedule))]
+public static class ScheduleConditionalUpdatePatch {
+
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(Schedule.SetGroup))]
+    private static bool SetGroupPrefix(Schedule __instance, int idx, ScheduleGroup group) {
+        if (idx < 0 || idx >= __instance.blocks.Count)
+            return false;
+
+        return __instance.blocks[idx].GroupId != group.Id;
+    }
+
+}
