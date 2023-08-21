04c0ca70e0a77a7bc2546d0648cadce70cf20a48
ewoudje <7384674+ewoudje@users.noreply.github.com>
2023-08-21 11:51:23 +0200

    Cursor Lerp Smoothing

--- MultiplayerMod/Multiplayer/State/PlayerState.cs
+++ MultiplayerMod/Multiplayer/State/PlayerState.cs
@@ -6,8 +6,28 @@ namespace MultiplayerMod.Multiplayer.State;
 [Serializable]
 public class PlayerState {
     public IPlayerIdentity Player { get; }
-    public Vector2 CursorPosition { get; set; }
+
+    public Vector2 CursorPosition {
+        get {
+            var updateDelta = newCursorTime - prevCursorTime;
+            var timeDiff = (Time.time - newCursorTime) / updateDelta;
+            return Vector2.Lerp(prevCursorLocation, newCursorLocation, timeDiff);
+        }
+
+        set {
+            prevCursorLocation = newCursorLocation;
+            prevCursorTime = newCursorTime;
+            newCursorLocation = value;
+            newCursorTime = Time.time;
+        }
+    }
+
     public bool WorldSpawned { get; set; }
+
+    private Vector2 prevCursorLocation;
+    private Vector2 newCursorLocation;
+    private float prevCursorTime;
+    private float newCursorTime;
 
     public PlayerState(IPlayerIdentity player) {
         Player = player;
