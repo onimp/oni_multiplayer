7354152d8a00a5d103eb6d1dcbf4b672c17dd887
Denis Pakhorukov <xpolycone@gmail.com>
2023-09-06 02:16:04 +0300

    #220 Change dependencies compatibility binding

--- src/MultiplayerMod/ModRuntime/StaticCompatibility/Dependencies.cs
+++ src/MultiplayerMod/ModRuntime/StaticCompatibility/Dependencies.cs
@@ -4,9 +4,9 @@ namespace MultiplayerMod.ModRuntime.StaticCompatibility;
 
 public static class Dependencies {
 
-    private static readonly DependencyContainer container = Runtime.Instance.Dependencies;
+    private static DependencyContainer Container => Runtime.Instance.Dependencies;
 
-    public static T Inject<T>(T instance) where T : notnull => container.Inject(instance);
-    public static T Get<T>() where T : notnull => container.Get<T>();
+    public static T Inject<T>(T instance) where T : notnull => Container.Inject(instance);
+    public static T Get<T>() where T : notnull => Container.Get<T>();
 
 }
