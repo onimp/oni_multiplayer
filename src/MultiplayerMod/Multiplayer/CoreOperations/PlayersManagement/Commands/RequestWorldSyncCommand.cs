960789f538fbede6ff40446f7ccbf3d8927066e2
Denis Pakhorukov <xpolycone@gmail.com>
2023-09-25 18:40:44 +0300

    Fix world sync request command

--- src/MultiplayerMod/Multiplayer/CoreOperations/PlayersManagement/Commands/RequestWorldSyncCommand.cs
+++ src/MultiplayerMod/Multiplayer/CoreOperations/PlayersManagement/Commands/RequestWorldSyncCommand.cs
@@ -4,6 +4,7 @@ using MultiplayerMod.Multiplayer.Commands;
 namespace MultiplayerMod.Multiplayer.CoreOperations.PlayersManagement.Commands;
 
 [Serializable]
+[MultiplayerCommand(ExecuteOnServer = true)]
 public class RequestWorldSyncCommand : MultiplayerCommand {
 
     public override void Execute(MultiplayerCommandContext context) {
