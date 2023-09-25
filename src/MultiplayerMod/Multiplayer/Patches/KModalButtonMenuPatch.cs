11e23c3068b98ff60aea89f754ac0f117148154a
Denis Pakhorukov <xpolycone@gmail.com>
2023-09-26 00:05:06 +0300

    #36 Disable game pause on a modal screen open

---
+++ src/MultiplayerMod/Multiplayer/Patches/KModalButtonMenuPatch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using JetBrains.Annotations;
+using MultiplayerMod.Core.Patch;
+using MultiplayerMod.ModRuntime;
+using MultiplayerMod.ModRuntime.Context;
+
+namespace MultiplayerMod.Multiplayer.Patches;
+
+[UsedImplicitly]
+[HarmonyPatch(typeof(KModalButtonMenu))]
+public class KModalButtonMenuPatch {
+
+    [HarmonyTranspiler, UsedImplicitly]
+    [HarmonyPatch("OnShow")]
+    private static IEnumerable<CodeInstruction> OnShow(IEnumerable<CodeInstruction> instructions) {
+        using var source = instructions.GetEnumerator();
+        var result = new List<CodeInstruction>();
+        var instanceGetter = AccessTools.PropertyGetter(
+            typeof(SpeedControlScreen),
+            nameof(SpeedControlScreen.Instance)
+        );
+
+        result.AddConditional(source, it => it.Calls(instanceGetter));
+        result.AddConditional(source, it => it.Branches(out _));
+        var jumpOutOfSpeedControlBlock = result.Last();
+
+        result.Add(CodeInstruction.Call(typeof(KModalButtonMenuPatch), nameof(ShouldPause)));
+        result.Add(jumpOutOfSpeedControlBlock);
+
+        result.AddConditional(source, _ => false);
+        return result;
+    }
+
+    private static bool ShouldPause() => !Runtime.Instance.Dependencies.Get<ExecutionLevelManager>()
+        .LevelIsActive(ExecutionLevel.Game);
+
+}
