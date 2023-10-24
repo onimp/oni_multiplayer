fe35618ef7516b70c44ac3a5bbef82775645fb3c
Denis Pakhorukov <xpolycone@gmail.com>
2023-10-25 02:26:57 +0300

    #243 Add destroy on player leave component

---
+++ src/MultiplayerMod/Multiplayer/Components/AssignedMultiplayerPlayer.cs
@@ -0,0 +1,10 @@
+﻿using MultiplayerMod.Multiplayer.Players;
+using UnityEngine;
+
+namespace MultiplayerMod.Multiplayer.Components;
+
+public class AssignedMultiplayerPlayer : MonoBehaviour {
+
+    public MultiplayerPlayer Player { get; set; } = null!;
+
+}
---
+++ src/MultiplayerMod/Multiplayer/Components/DestroyOnPlayerLeave.cs
@@ -0,0 +1,28 @@
+﻿using MultiplayerMod.Core.Dependency;
+using MultiplayerMod.Core.Events;
+using MultiplayerMod.Core.Unity;
+using MultiplayerMod.Multiplayer.Players.Events;
+
+namespace MultiplayerMod.Multiplayer.Components;
+
+public class DestroyOnPlayerLeave : MultiplayerKMonoBehaviour {
+
+    [InjectDependency]
+    private readonly EventDispatcher events = null!;
+
+    [MyCmpReq]
+    private readonly AssignedMultiplayerPlayer playerComponent = null!;
+
+    private EventSubscription subscription = null!;
+
+    protected override void OnSpawn() {
+        var player = playerComponent.Player;
+        subscription = events.Subscribe<PlayerLeftEvent>(@event => {
+            if (@event.Player == player)
+                DestroyImmediate(gameObject);
+        });
+    }
+
+    protected override void OnForcedCleanUp() => subscription.Cancel();
+
+}
