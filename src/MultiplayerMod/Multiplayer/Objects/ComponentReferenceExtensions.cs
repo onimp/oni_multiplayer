b86727fd6adc2049440f457d71da07b4965db8d7
Denis Pakhorukov <xpolycone@gmail.com>
2023-11-07 00:41:37 +0200

    #297 Cleanup component reference extensions
    Since we should assign multiplayer id to movable objects there's no
    reason to detect if an object is movable using it's type.

--- src/MultiplayerMod/Multiplayer/Objects/ComponentReferenceExtensions.cs
+++ src/MultiplayerMod/Multiplayer/Objects/ComponentReferenceExtensions.cs
@@ -1,34 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using MultiplayerMod.Multiplayer.Objects.Reference;
 
 namespace MultiplayerMod.Multiplayer.Objects;
 
 public static class ComponentReferenceExtensions {
 
-    private static readonly HashSet<Type> movableObjectTypes = new() {
-        typeof(MinionAssignablesProxy),
-        typeof(MinionResume),
-        typeof(Schedulable),
-        typeof(ConsumableConsumer),
-        typeof(ChoreConsumer)
-    };
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ComponentReference GetReference(this KMonoBehaviour component) => new(
+        component.gameObject.GetReference(),
+        component.GetType()
+    );
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ComponentReference GetReference(this KMonoBehaviour component)
-        => component.Movable()
-            ? new ComponentReference(component.gameObject.GetMultiplayerReference(), component.GetType())
-            : new ComponentReference(component.gameObject.GetGridReference(), component.GetType());
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ComponentReference<T> GetReference<T>(this T component) where T : KMonoBehaviour
-        => component.Movable()
-            ? new ComponentReference<T>(component.gameObject.GetMultiplayerReference())
-            : new ComponentReference<T>(component.gameObject.GetGridReference());
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static bool Movable(this KMonoBehaviour kMonoBehaviour) =>
-        movableObjectTypes.Contains(kMonoBehaviour.GetType());
+    public static ComponentReference<T> GetReference<T>(this T component) where T : KMonoBehaviour => new(
+        component.gameObject.GetReference()
+    );
 
 }
