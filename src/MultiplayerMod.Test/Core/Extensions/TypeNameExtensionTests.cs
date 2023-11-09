ec1c5958bd2a36d7f1926722a70cc4dee6fb4c8b
Denis Pakhorukov <xpolycone@gmail.com>
2023-11-09 22:41:53 +0200

    #297 Add type pretty name builder

---
+++ src/MultiplayerMod.Test/Core/Extensions/TypeNameExtensionTests.cs
@@ -0,0 +1,81 @@
+﻿using MultiplayerMod.Core.Extensions;
+using NUnit.Framework;
+
+// ReSharper disable UnusedTypeParameter
+
+namespace MultiplayerMod.Test.Core.Extensions;
+
+[TestFixture]
+public class TypeNameExtensionTests {
+
+    [Test]
+    public void SimpleGenericClassName() {
+        var name = typeof(SimpleGenericClass<int>).GetPrettyName();
+        Assert.AreEqual(expected: "SimpleGenericClass<Int32>", actual: name);
+    }
+
+    [Test]
+    public void SimpleGenericInnerClassName() {
+        var name = typeof(SimpleGenericClass<int>.InnerClass<string, long>).GetPrettyName();
+        Assert.AreEqual(expected: "SimpleGenericClass<Int32>.InnerClass<String, Int64>", actual: name);
+    }
+
+    [Test]
+    public void SimpleGenericNestedClassName() {
+        var name = typeof(SimpleGenericClass<int>.InnerClass<string, long>.NestedClass).GetPrettyName();
+        Assert.AreEqual(expected: "SimpleGenericClass<Int32>.InnerClass<String, Int64>.NestedClass", actual: name);
+    }
+
+    [Test]
+    public void SimpleGenericNestedClassNameWithNamespace() {
+        var name = typeof(SimpleGenericClass<int>.InnerClass<string, long>.NestedClass)
+            .GetPrettyName(TypeNameOptions.IncludeNamespace);
+        Assert.AreEqual(
+            expected: "MultiplayerMod.Test.Core.Extensions.SimpleGenericClass<System.Int32>.InnerClass<System.String, System.Int64>.NestedClass",
+            actual: name
+        );
+    }
+
+    [Test]
+    public void SimpleGenericNestedArrayClassName() {
+        var name = typeof(SimpleGenericClass<int[]>.InnerClass<string, long>.NestedClass[]).GetPrettyName();
+        Assert.AreEqual(expected: "SimpleGenericClass<Int32[]>.InnerClass<String, Int64>.NestedClass[]", actual: name);
+    }
+
+    [Test]
+    public void SimpleClassName() {
+        var name = typeof(SimpleClass).GetPrettyName();
+        Assert.AreEqual(expected: "SimpleClass", actual: name);
+    }
+
+    [Test]
+    public void SimpleArrayClassName() {
+        var name = typeof(SimpleClass[]).GetPrettyName();
+        Assert.AreEqual(expected: "SimpleClass[]", actual: name);
+    }
+
+    [Test]
+    public void SimpleInnerClassName() {
+        var name = typeof(SimpleClass.InnerClass<string, long>).GetPrettyName();
+        Assert.AreEqual(expected: "SimpleClass.InnerClass<String, Int64>", actual: name);
+    }
+
+    [Test]
+    public void SimpleNestedClassName() {
+        var name = typeof(SimpleClass.InnerClass<string, long>.NestedClass).GetPrettyName();
+        Assert.AreEqual(expected: "SimpleClass.InnerClass<String, Int64>.NestedClass", actual: name);
+    }
+
+}
+
+public class SimpleClass {
+    public class InnerClass<InnerType1, InnerType2> {
+        public class NestedClass { }
+    }
+}
+
+public class SimpleGenericClass<Type> {
+    public class InnerClass<InnerType1, InnerType2> {
+        public class NestedClass { }
+    }
+}
---
+++ src/MultiplayerMod/Core/Extensions/TypeNameExtension.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace MultiplayerMod.Core.Extensions;
+
+[Flags]
+public enum TypeNameOptions {
+    IncludeNamespace = 0b1,
+    None = 0
+}
+
+public static class TypeNameExtension {
+
+    public static string GetPrettyName(this Type type, TypeNameOptions options = TypeNameOptions.None) {
+        return GetTypeName(type, options);
+    }
+
+    private static string GetTypeName(Type type, TypeNameOptions options) {
+        return GetTypeName(type, options, type.GetGenericArguments(), 0);
+    }
+
+    private static string GetTypeName(Type type, TypeNameOptions options, Type[] genericArguments, int endOffset) {
+        if (type.IsArray)
+            return $"{GetTypeName(type.GetElementType()!, options)}[]";
+
+        var includeNamespace = (options & TypeNameOptions.IncludeNamespace) != 0 && !type.IsNested;
+        var name = includeNamespace ? $"{type.Namespace}.{type.Name}" : type.Name;
+
+        if (!type.IsGenericType)
+            return name;
+
+        var genericParametersCount = type.GetGenericTypeDefinition().GetGenericArguments().Length;
+        if (type.IsNested) {
+            genericParametersCount -= type.GetGenericTypeDefinition().DeclaringType!.GetGenericArguments().Length;
+            var declaringTypeName = GetTypeName(
+                type.DeclaringType!,
+                options,
+                genericArguments,
+                endOffset + genericParametersCount
+            );
+            name = $"{declaringTypeName}.{name}";
+        }
+
+        if (genericParametersCount == 0)
+            return name;
+
+        name = name.Substring(0, name.LastIndexOf("`", StringComparison.InvariantCulture));
+        var offset = genericArguments.Length - (endOffset + genericParametersCount);
+        var arguments = genericArguments.Skip(offset).Take(genericParametersCount);
+        name = $"{name}<{string.Join(", ", arguments.Select(it => GetTypeName(it, options)))}>";
+
+        return name;
+    }
+
+}
