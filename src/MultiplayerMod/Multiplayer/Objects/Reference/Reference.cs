c7cdaacb0f9428c7fa15979f0b6ed7b54ac4d388
Denis Pakhorukov <xpolycone@gmail.com>
2024-04-04 02:41:18 +0300

    Fix TypedReference serialization

--- src/MultiplayerMod/Multiplayer/Objects/Reference/Reference.cs
+++ src/MultiplayerMod/Multiplayer/Objects/Reference/Reference.cs
@@ -11,6 +11,7 @@ public interface Reference {
     public abstract object? Resolve();
 }
 
+[Serializable]
 public abstract class TypedReference<T> : Reference<T>, Reference {
     public abstract T? Resolve();
     object? Reference.Resolve() => Resolve();
