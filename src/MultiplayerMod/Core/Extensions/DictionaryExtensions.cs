d954cae0f98f04cfe40b9e3dff863168f150624d
Denis Pakhorukov <xpolycone@gmail.com>
2024-04-23 02:49:09 +0300

    Add dictionary extensions

---
+++ src/MultiplayerMod/Core/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace MultiplayerMod.Core.Extensions;
+
+public static class DictionaryExtensions {
+
+    public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        where TValue : new()
+    {
+        if (dictionary.TryGetValue(key, out var val))
+            return val;
+
+        val = new TValue();
+        dictionary.Add(key, val);
+        return val;
+    }
+
+}
