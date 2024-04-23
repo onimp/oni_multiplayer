e8353d6f7c303518edc2f46e6d8b221082d11c68
Denis Pakhorukov <xpolycone@gmail.com>
2024-04-24 00:45:44 +0300

    Replace conditional DSL with multiplayer game dependency access
    Conditional actions aren't required as the initialization phase
    happens in-game thus conditions can be inlined into the configuration.

--- src/MultiplayerMod/Multiplayer/StateMachines/Configuration/Dsl/StateMachineConfigurerDsl.cs
+++ src/MultiplayerMod/Multiplayer/StateMachines/Configuration/Dsl/StateMachineConfigurerDsl.cs
@@ -9,14 +9,7 @@ using MultiplayerMod.ModRuntime.StaticCompatibility;
 
 namespace MultiplayerMod.Multiplayer.StateMachines.Configuration.Dsl;
 
-public class StateMachineConfigurerDsl {
-    public static class Conditions {
-        public static Func<bool> MultiplayerMode(MultiplayerMode mode) =>
-            () => Dependencies.Get<MultiplayerGame>().Mode == mode;
-    }
-}
-
-public class StateMachineConfigurerDsl<TStateMachine, TStateMachineInstance, TMaster, TDef> : StateMachineConfigurerDsl
+public class StateMachineConfigurerDsl<TStateMachine, TStateMachineInstance, TMaster, TDef>
     where TStateMachine : GameStateMachine<TStateMachine, TStateMachineInstance, TMaster, TDef>
     where TStateMachineInstance : GameStateMachine<TStateMachine, TStateMachineInstance, TMaster, TDef>.GameInstance
     where TMaster : IStateMachineTarget
@@ -24,29 +17,15 @@ public class StateMachineConfigurerDsl<TStateMachine, TStateMachineInstance, TMa
 
     private readonly List<StateMachineConfigurationAction> actions = [];
 
+    private readonly Lazy<MultiplayerGame> game = new(Dependencies.Get<MultiplayerGame>);
+    public MultiplayerGame Game => game.Value;
+
     public void Phase(StateMachineConfigurationPhase phase, System.Action action) =>
         actions.Add(new StateMachineConfigurationAction(phase, action));
 
     public void PreConfigure(System.Action action) => Phase(StateMachineConfigurationPhase.PreConfiguration, action);
 
     public void PostConfigure(System.Action action) => Phase(StateMachineConfigurationPhase.PostConfiguration, action);
-
-    public void Conditional(
-        Func<bool> condition,
-        Action<StateMachineConfigurerDsl<TStateMachine, TStateMachineInstance, TMaster, TDef>> scopedAction
-    ) {
-        var scopedDsl = new StateMachineConfigurerDsl<TStateMachine, TStateMachineInstance, TMaster, TDef>();
-        scopedAction(scopedDsl);
-        var scopedActions = scopedDsl.actions.Select(
-            it => it with {
-                Action = () => {
-                    if (condition())
-                        it.Action();
-                }
-            }
-        );
-        actions.AddRange(scopedActions);
-    }
 
     public void Suppress(Expression<System.Action> expression) {
         var customizer = Dependencies.Get<ControlFlowCustomizer>();
