265f70945df408239fd726b383bb2ce81bb3a8d7
Denis Pakhorukov <xpolycone@gmail.com>
2024-04-24 02:24:45 +0300

    Fix control flow customizer

---
+++ src/MultiplayerMod.Test/Core/Patch/ControlFlowCustomizerTests.cs
@@ -0,0 +1,95 @@
+﻿using HarmonyLib;
+using MultiplayerMod.Core.Patch;
+using NUnit.Framework;
+
+namespace MultiplayerMod.Test.Core.Patch;
+
+[TestFixture]
+public class ControlFlowCustomizerTests {
+
+    private static readonly Harmony harmony = new("Test");
+    private static readonly ControlFlowCustomizer customizer = new(harmony);
+
+    [OneTimeTearDown]
+    public static void TearDown() => harmony.UnpatchAll("Test");
+
+    [Test]
+    public void MustDisableMethodWithVoidReturn() {
+        Container<int> container = new();
+        container.SetValue(1);
+        Assert.AreEqual(expected: 1, actual: container.Value);
+        var setValue = typeof(Container<int>).GetMethod(nameof(Container<int>.SetValue))!;
+        customizer.DisableMethod(container, setValue);
+        container.SetValue(2);
+        Assert.AreEqual(expected: 1, actual: container.Value);
+        customizer.EnableMethods(container);
+        container.SetValue(3);
+        Assert.AreEqual(expected: 3, actual: container.Value);
+    }
+
+    [Test]
+    public void MustDisableMethodWithValueTypeReturn() {
+        Container<int> container = new();
+        Assert.AreEqual(expected: 1, actual: container.SetValueRet(1));
+        var setValue = typeof(Container<int>).GetMethod(nameof(Container<int>.SetValueRet))!;
+        customizer.DisableMethod(container, setValue);
+        Assert.AreEqual(expected: 0, actual: container.SetValueRet(2));
+        Assert.AreEqual(expected: 1, actual: container.Value);
+        customizer.EnableMethods(container);
+        Assert.AreEqual(expected: 3, actual: container.SetValueRet(3));
+        Assert.AreEqual(expected: 3, actual: container.Value);
+    }
+
+    [Test]
+    public void MustDisableMethodWithValueTypeReturnAndOverrideReturnValue() {
+        Container<int> container = new();
+        Assert.AreEqual(expected: 1, actual: container.SetValueRet(1));
+        var setValue = typeof(Container<int>).GetMethod(nameof(Container<int>.SetValueRet))!;
+        customizer.DisableMethod(container, setValue, 10);
+        Assert.AreEqual(expected: 10, actual: container.SetValueRet(2));
+        Assert.AreEqual(expected: 1, actual: container.Value);
+        customizer.EnableMethods(container);
+        Assert.AreEqual(expected: 3, actual: container.SetValueRet(3));
+        Assert.AreEqual(expected: 3, actual: container.Value);
+    }
+
+    [Test]
+    public void MustDisableMethodWithNullableTypeReturn() {
+        Container<string> container = new();
+        Assert.AreEqual(expected: "Earth", actual: container.SetValueRet("Earth"));
+        var setValue = typeof(Container<string>).GetMethod(nameof(Container<string>.SetValueRet))!;
+        customizer.DisableMethod(container, setValue);
+        Assert.AreEqual(expected: null, actual: container.SetValueRet("Mars"));
+        Assert.AreEqual(expected: "Earth", actual: container.Value);
+        customizer.EnableMethods(container);
+        Assert.AreEqual(expected: "Jupiter", actual: container.SetValueRet("Jupiter"));
+        Assert.AreEqual(expected: "Jupiter", actual: container.Value);
+    }
+
+    [Test]
+    public void MustDisableMethodWithNullableTypeReturnAndOverrideReturnValue() {
+        Container<string> container = new();
+        Assert.AreEqual(expected: "Earth", actual: container.SetValueRet("Earth"));
+        var setValue = typeof(Container<string>).GetMethod(nameof(Container<string>.SetValueRet))!;
+        customizer.DisableMethod(container, setValue, "Mercury");
+        Assert.AreEqual(expected: "Mercury", actual: container.SetValueRet("Mars"));
+        Assert.AreEqual(expected: "Earth", actual: container.Value);
+        customizer.EnableMethods(container);
+        Assert.AreEqual(expected: "Jupiter", actual: container.SetValueRet("Jupiter"));
+        Assert.AreEqual(expected: "Jupiter", actual: container.Value);
+    }
+
+    class Container<T> {
+        public T? Value { get; private set; }
+
+        public virtual void SetValue(T value) {
+            Value = value;
+        }
+
+        public virtual T SetValueRet(T value) {
+            Value = value;
+            return Value;
+        }
+    }
+
+}
--- src/MultiplayerMod/Core/Patch/ControlFlowCustomizer.cs
+++ src/MultiplayerMod/Core/Patch/ControlFlowCustomizer.cs
@@ -1,26 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using HarmonyLib;
 using JetBrains.Annotations;
-using MultiplayerMod.Core.Events;
 
 namespace MultiplayerMod.Core.Patch;
 
-[Core.Dependency.Dependency, UsedImplicitly]
-public class ControlFlowCustomizer {
-
-    private readonly Harmony harmony;
-
-    public ControlFlowCustomizer(EventDispatcher events, Harmony harmony) {
-        this.harmony = harmony;
-    }
+[Dependency.Dependency, UsedImplicitly]
+public class ControlFlowCustomizer(Harmony harmony) {
 
     private static readonly ConditionalWeakTable<object, Dictionary<MethodInfo, object?>> extents = new();
     private static readonly HashSet<MethodInfo> patchedMethods = [];
 
-    public void DisableMethod<T>(T instance, MethodInfo method) where T : notnull =>
-        DisableMethod<T, object?>(instance, method);
+    public void DisableMethod<T>(T instance, MethodInfo method) where T : notnull => DisableMethod<T, object?>(
+        instance,
+        method,
+        method.ReturnType.IsValueType
+            ? method.ReturnType != typeof(void) ? Activator.CreateInstance(method.ReturnType) : null
+            : null
+    );
 
     public void DisableMethod<T, R>(T instance, MethodInfo method, R? returnValue = default) where T : notnull {
         if (!patchedMethods.Contains(method)) {
@@ -54,13 +53,13 @@ public class ControlFlowCustomizer {
     }
 
     [UsedImplicitly]
-    private static bool AdviceWithRet<T>(MethodInfo __originalMethod, T __instance, ref T? __result) where T : notnull {
+    private static bool AdviceWithRet<R>(MethodInfo __originalMethod, object __instance, ref R? __result) {
         if (!extents.TryGetValue(__instance, out var methods))
             return true;
 
         var skip = methods.TryGetValue(__originalMethod, out var returnValue);
         if (skip)
-            __result = (T?) returnValue;
+            __result = (R?) returnValue;
         return !skip;
     }
 
