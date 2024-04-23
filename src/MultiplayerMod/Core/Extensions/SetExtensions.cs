b03b44fed26c47e689cd6231330bfd5392a44f16
Denis Pakhorukov <xpolycone@gmail.com>
2024-04-23 03:00:37 +0300

    Add set extensions

---
+++ src/MultiplayerMod/Core/Extensions/SetExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MultiplayerMod.Core.Extensions;
+
+public static class SetExtensions {
+
+    public static void AddRange<T>(this ISet<T> set, IEnumerable<T> items) => items.ForEach(it => set.Add(it));
+
+}
