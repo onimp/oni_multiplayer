660b60aa7b2a5b021695a99ad4ef9ceeaa8efdd6
Denis Pakhorukov <denis@polycone.dev>
2024-06-09 02:49:50 +0300

    Disable attack chore instantiation on client

--- src/MultiplayerMod/Multiplayer/Chores/ChoresMultiplayerConfiguration.cs
+++ src/MultiplayerMod/Multiplayer/Chores/ChoresMultiplayerConfiguration.cs
@@ -1,4 +1,5 @@
 using MultiplayerMod.Multiplayer.Chores.Synchronizers;
+using MultiplayerMod.Multiplayer.StateMachines.Configuration;
 using static MultiplayerMod.Multiplayer.Chores.Dsl.ChoresConfigurationDsl;
 
 namespace MultiplayerMod.Multiplayer.Chores;
@@ -6,7 +7,15 @@ namespace MultiplayerMod.Multiplayer.Chores;
 public static class ChoresMultiplayerConfiguration {
 
     public static readonly ChoreConfiguration[] Configuration = [
-        Synchronize<AttackChore>(),
+        // @formatter:off
+        Synchronize<AttackChore, AttackChore.States, AttackChore.StatesInstance>(root => {
+            root.Inline(new StateMachineConfigurerDsl<ThreatMonitor, ThreatMonitor.Instance, IStateMachineTarget, ThreatMonitor.Def>(configurer => {
+                configurer.PreConfigure(MultiplayerMode.Client, pre => {
+                    pre.Suppress(() => pre.StateMachine.threatened.duplicant.ShouldFight.ToggleChore(null, null));
+                });
+            }));
+        }),
+        // @formatter:on
         UseSynchronizer<IdleChore, IdleChore.States, IdleChore.StatesInstance, IdleChoreSynchronizer>(),
         UseSynchronizer<MoveToSafetyChore, MoveToSafetyChore.States, MoveToSafetyChore.StatesInstance, MoveToSafetyChoreSynchronizer>(),
         UseStateMachineSynchronizer<IdleStates, IdleStates.Instance, IdleStates.Def, IdleStatesSynchronizer>()
