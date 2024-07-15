926abd3b5f177b12ab4cc6d6aa537dcdb09684b7
Denis Pakhorukov <denis@polycone.dev>
2024-07-16 01:34:03 +0300

    Add death sync

---
+++ src/MultiplayerMod.Test/Multiplayer/StateMachines/RuntimeTools/StateMachineRuntimeToolsTests.cs
@@ -0,0 +1,17 @@
+﻿using MultiplayerMod.Multiplayer.StateMachines.RuntimeTools;
+using NUnit.Framework;
+
+namespace MultiplayerMod.Test.Multiplayer.StateMachines.RuntimeTools;
+
+[TestFixture]
+public class StateMachineRuntimeToolsTests : StateMachinesTest {
+
+    [Test]
+    public void ControllerIsResolved() {
+        var instance = CreateStateMachineInstance<StateMachineContextRuntimeToolsTests.TestStateMachine.Instance>();
+        var runtime = StateMachineRuntimeTools.Get(instance);
+        var controller = runtime.GetController();
+        Assert.That(controller, Is.Not.Null);
+    }
+
+}
---
+++ src/MultiplayerMod/Multiplayer/StateMachines/Commands/SetParameterValue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MultiplayerMod.Multiplayer.Commands;
+using MultiplayerMod.Multiplayer.Objects.Extensions;
+using MultiplayerMod.Multiplayer.Objects.Reference;
+using MultiplayerMod.Multiplayer.StateMachines.RuntimeTools;
+
+namespace MultiplayerMod.Multiplayer.StateMachines.Commands;
+
+[Serializable]
+public class SetParameterValue : MultiplayerCommand {
+
+    private ComponentReference<StateMachineController> controllerReference;
+    private Type stateMachineInstanceType;
+    private int parameterIndex;
+    private object? value;
+
+    public SetParameterValue(StateMachine.Instance smi, StateMachine.Parameter parameter, object? value) {
+        var runtime = StateMachineRuntimeTools.Get(smi);
+        controllerReference = runtime.GetController().GetReference();
+        stateMachineInstanceType = smi.GetType();
+        parameterIndex = parameter.idx;
+        this.value = ArgumentUtils.WrapObject(value);
+    }
+
+    public override void Execute(MultiplayerCommandContext context) {
+        var instance = controllerReference.Resolve().GetSMI(stateMachineInstanceType);
+        var parameterContext = instance.parameterContexts[parameterIndex];
+        var parameterValue = ArgumentUtils.UnWrapObject(value);
+        StateMachineContextRuntimeTools.Get(parameterContext).Set(instance, parameterValue);
+    }
+
+}
